f46813710460751608e004a946641242d8c40101
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2026-01-04 00:26:06 +0300

    HomeRouter

---
+++ VIPER/Features/Home/Navigation/HomeRouter.swift
@@ -0,0 +1,21 @@
+//
+//  HomeRouter.swift
+//  VIPER
+//
+//  Created by rico on 4.01.2026.
+//
+
+import UIKit
+
+@MainActor
+final class HomeRouter: HomeRouterProtocol {
+    private weak var viewController: UIViewController?
+    
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+    
+    func navigateToDetail(with character: RMCharacter) {
+        print("GO Detail")
+    }
+}
